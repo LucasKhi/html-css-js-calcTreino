0233a4bac4ce72826a0cd69e50cfd8d294599e80
Luca Souza <89982608+LucasKhi@users.noreply.github.com>
2021-12-15 17:45:19 -0300

    operacoes-corrigida

---
+++ logica.js
@@ -0,0 +1,15 @@
+function calcula(){
+    let numString = document.getElementById("display-calculadora").value
+    let resposta = eval(numString)
+    document.getElementById("display-calculadora").value = resposta
+} 
+
+function clr(){
+    document.getElementById("display-calculadora").value = ""
+}
+
+function dis(valor){
+    document.getElementById("display-calculadora").value += valor
+    if (value ==)
+}
+
